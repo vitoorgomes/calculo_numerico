bf3b6e043930220df1d7e9b66c99ed20807234c7
Vitor Gomes <vitorgomes2007@hotmail.com>
2020-10-06 20:34:08 -0300

    mnr updated

--- newton-rhapson.js
+++ newton-rhapson.js
@@ -1,25 +1,18 @@
-const { derivative, pow, cosh, e } = require('../node_modules/mathjs');
+const { derivative, sinh, cosh, e, pow } = require('../node_modules/mathjs');
 
-const derivada = derivative('cosh(x) - 2*pow(e, -0.3*x)', 'x');
+const derivada = derivative('-(1/x)*(sinh(-30*x) - sinh(15*x))', 'x');
 
 function funcDerivada(x) {                                  // Calcular a f(x) com a fórumla de Newton-Raphson
     return x - (funcFX(x)/ derivada.evaluate({x}))
 }
 
 function funcFX(x) {                                        // Método para calcular o f(x) com a raiz
-    return cosh(x) - 2*pow(e, -0.3*x);                      // encontrada
+    return -(1/x)*(sinh(-30*x) - sinh(15*x)) - 120;         // encontrada
 }
 
-function corrSinal(fx) {                                    // Como não há uma função nativa para módulo
-    if (fx < 0) {                                           // apenas um ajudante para conferir o valor
-        return fx*(-1);                                     // da raíz e se for negativo apenas multiplicar
-    }                                                       // por -1 e inverter o sinal
-    return fx;
-}
-
-function calcularMNR(a, b, p) {
+function calcularMNR(a, p) {
     let k = 1;
-    let raizMedia = (a + b) / 2;                            
+    let raizMedia = a;                 
     let testeFim = 0;                                       
 
     if (derivada.toString() != '0') {                       // Antes de começar as iterações, confere se
@@ -28,8 +21,7 @@ function calcularMNR(a, b, p) {
             raizMedia = funcDerivada(raizMedia);
 
             testeFim = funcFX(raizMedia);
-            if (corrSinal(testeFim) < p) {
-                raizMedia = testeFim;
+            if (Math.abs(testeFim) < p) {
                 break;
             }
         }
@@ -40,4 +32,4 @@ function calcularMNR(a, b, p) {
     }
 
 }
-console.log(calcularMNR(0.5,1.5,0.001));
+console.log(calcularMNR(0.12,0.01));
